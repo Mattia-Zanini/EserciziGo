ea724b6b432830214ff1852e03909b787c96243b
Mattia-Zanini <mattzanini2@gmail.com>
2025-02-26 08:27:29 +0100

    .

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var y int = 3 // al package level posso definire le variabili solo in questo modo
 
 // Posso definire più variabili al package level senza utilizzando un blocco 'var', queste variabili
-// NON sono correlate, questa NON è una struct
+// NON devono per forza essere correlate, questa NON è una struct, sto semplicemente evitando di
+// scrivere "var" per definire ogni variabile, scrivendolo una sola volta
 var (
 	nome        string  = "Mario"
 	cognome     string  = "Rossi"
@@ -20,6 +24,12 @@ var pluto int = 6
 // in minuscolo sono visualizzabili SOLO dal pacchetto main
 
 var P int = 0 // mentre questa variabile è visualizzabile anche dagli altri pacchetti globalmente
+// Basta che solo la prima lettere sia maiuscola perchè sia visualizzabile globalmente dagli altri pacchetti
+var Papere int = -1
+
+// Inoltre c'è una convenzione (è preferibile rispettare questa cosa ma non è necessaria) che le
+// variabili con nomi lunghi rappresentano variabile che vivono per più tempo, quindi una variabile
+// con un nome corto 'dovrebbe' vivere meno rispetto ad una variabile che ha un nome lungo e descrittivo
 
 func main() {
 	fmt.Printf("Hello World!\n")
@@ -47,4 +57,42 @@ func main() {
 	fmt.Printf("pluto: %v\n", pluto) // stampa 6
 	var pluto int = 13
 	fmt.Printf("pluto: %v\n", pluto) // stampa 13
+
+	// Esistono anche metodi per convertire un tipo in un altro
+	b := float32(i)
+	fmt.Printf("%v, %T\n", b, b)
+	b2 := int(b) // Questa conversione fa perdere informazioni, ad esempio le cifre dopo la virgola
+	fmt.Println(b2)
+
+	b3 := string(i) // Questa conversione converte i in una stringa, però in formato ASCII
+	fmt.Printf("%v, %T\n", b3, b3)
+
+	// Per convertire i in una stringa vera e propria, quindi che diventi "42" allora bisogna usare
+	// il metodo strconv.Itoa() del pacchetto strconv.
+	// Itoa() sta per "I" -> integer, to "a" -> ascii
+	s := strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", s, s)
+
+	// NON si possono fare conversioni implicite, bisogna SEMPRE specificare la conversione
+
+	// Si può definire una variabile booleana per esempio in questo modo
+	bo := 1 == 2
+	fmt.Printf("%v, %T\n", bo, bo)
+
+	// Quando una variabile primitiva è definita essa è inizializzata a 0
+	var boo bool // viene inizializzata a false (= 0)
+	fmt.Printf("%v, %T\n", boo, boo)
+
+	// Esistono vari tipi di interi: int8, int16, int32, int64, uint8, uint16, uint32
+	// Però non si possono fare operazioni tra interi diversi, bisogna sempre specificare la conversione
+	// quindi bisogna convertire un intero nel tipo dell'altro
+	// var n1 int = 5
+	// var n2 int8 = 3
+	// fmt.Println(n1 + n2) // Dà errore, a meno che non converta uno dei due
+
+	// Operatori booleani:
+	// &	AND
+	// |	OR
+	// ^	OR esclusiva
+	// &^	AND NOT
 }
